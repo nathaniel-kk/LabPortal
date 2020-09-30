b590a0e8c407ab3c4e41cc3ed863ab51a6164f1f
bixuande <794776085@qq.com>
2020-09-30 12:04:33 +0800

    李忠政

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,21 @@ Route::prefix('auth')->namespace('Auth')->group(function () {
     Route::post('refresh', 'AuthController@refresh'); //刷新token
     Route::post('register', 'AuthController@registered'); //刷新token
 });
+Route::prefix('member')->namespace('Admin\MemberManage')->group(function(){
+    Route::post('accountadd','MemberController@accountAdd');//添加用户
+    Route::get('accountstate','MemberController@accountState');//禁用操作
+    Route::get('accountquery','MemberController@accountQuery');//用户查询
+    Route::get('accountexhibition','MemberController@accountExhibition');//数据展示
+});
+
+Route::prefix('sign')->namespace('Admin\MemberManage')->group(function (){
+    Route::get('signexhibition','SignController@signExhibition');//数据展示
+    Route::get('signquery','SignController@signQuery');//查询
+    Route::get('signdetails','SignController@signDetails');//详情展示
+    Route::post('signadd','SignController@signAdd');//增加用户
+
+});
+
+Route::prefix('web')->namespace('Admin\MemberManage')->group(function (){
+    Route::get('websitestate','SignController@websiteState');//总禁用
+});
